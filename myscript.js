0b97fe2fdaaaa809def88ac6e719c1538173b360
Homiya Nowshin <nowshingdr@protonmail.com>
2022-11-07 22:49:30 +0600

    Blah blah blah

---
+++ myscript.js
@@ -0,0 +1,7 @@
+alert("First exercise!");
+var deposit = prompt("what is your height in cm? ");
+alert("Your height is "+ deposit/100+"meter");
+console.log("Hey there,,,,,");
+console.log("2"===2);
+console.log(NaN == NaN);
+console.log(NaN === NaN);
